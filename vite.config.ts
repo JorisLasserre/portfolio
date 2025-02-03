425dce4e346b36ae0e65fc9180c7ba64bc73b484
Joris Lasserre <joris.lasserre@iut.u-bordeaux.fr>
2025-02-03 09:34:12 +0100

    Fix Netlify redirects

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/portfolio',
   plugins: [react()],
   build: {
-    outDir: 'dist',
-  }
+    outDir: "dist",
+    assetsDir: "assets",
+    rollupOptions: {
+      input: {
+        main: "index.html",
+      },
+    },
+  },
 });
-
